24b0d96e83f7b0878540d0a49d1fd2929fc1515a
Jathin Devaiah <jathindevaiah@Jathins-MacBook-Pro.local>
2021-02-01 13:05:14 -0500

    User Registration and Auth routes

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const userRoutes = require('./routes/api/users');
+const authRoutes = require('./routes/api/auth');
+const profileRoutes = require('./routes/api/profile');
+const postsRoutes = require('./routes/api/posts');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/posts', postsRoutes);
 
 app.listen(PORT,() => {
     console.log(`Server started on port ${PORT}`);
